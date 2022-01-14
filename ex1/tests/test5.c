7db19edb8dcdfdaf38bedd0a206d5ca56d895ac6
Rafa10 <maiordomundo3@gmail.com>
2022-01-14 11:46:47 +0000

    More stuff test1

--- ex1/tests/test5.c
+++ ex1/tests/test5.c
@@ -2,39 +2,40 @@
 #include <assert.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
-const char text1[] = "ABCDEFG";
-const unsigned size1 = sizeof(text1) - 1;
-const char text2[] = "1234";
-const unsigned size2 = sizeof(text2) - 1;
-const char text3[] = "pizza";
-const unsigned size3 = sizeof(text3) - 1;
+typedef struct {
+    const char *str;
+    const size_t sz;
+} ct_string_view;
 
-void *t1_func(void *arg) {
-    const int handle = *(int *)arg;
+typedef struct {
+    int handle;
+    int index;
+} handle_index;
 
-    for (int i = 0; i < 200; ++i)
-        assert(tfs_write(handle, text1, size1) == size1);
+#define THREAD_AMOUNT 6
 
-    return NULL;
-}
+ct_string_view views[THREAD_AMOUNT] = {
+    {.str = "ABCDEFG", .sz = sizeof("ABCDEFG") - 1},
+    {.str = "1234", .sz = sizeof("1234") - 1},
+    {.str = "pizza", .sz = sizeof("pizza") - 1},
+    {.str = "amogus", .sz = sizeof("amogus") - 1},
+    {.str = "SDFef", .sz = sizeof("SDFef") - 1},
+    {.str = "69aa420", .sz = sizeof("69aa420") - 1}
+};
 
-void *t2_func(void *arg) {
-    const int handle = *(int *)arg;
+void *t_func(void *arg) {
+    const handle_index *hidx = (handle_index *)arg;
 
-    for (int i = 0; i < 200; ++i)
-        assert(tfs_write(handle, text2, size2) == size2);
+    const int handle = hidx->handle;
+    const char *s = views[hidx->index].str;
+    const size_t sz = views[hidx->index].sz;
 
-    return NULL;
-}
-
-void *t3_func(void *arg) {
-    (void)arg;
-
-    const int handle = *(int *)arg;
-
-    for (int i = 0; i < 69; ++i)
-        assert(tfs_write(handle, text3, size3) == size3);
+    int amount = 69 + rand() % 420;
+    for (int i = 0; i < amount; ++i)
+        assert(tfs_write(handle, s, sz) == sz);
 
     return NULL;
 }
@@ -45,22 +46,26 @@ int test_write_interference(char *buf, size_t len) {
      * Else, there was interference. */
     while (buf != cap) {
         const size_t cur = (size_t)(cap - buf);
-        size_t val;
 
-        if (!strncmp(buf, text1, (val = cur < size1 ? cur : size1)))
-            buf += val;
-        else if (!strncmp(buf, text2, (val = cur < size2 ? cur : size2)))
-            buf += val;
-        else if (!strncmp(buf, text3, (val = cur < size3 ? cur : size3)))
-            buf += val;
-        else
+        int i;
+        for(i = 0; i < THREAD_AMOUNT; ++i) {
+            size_t val = cur < views[i].sz ? cur : views[i].sz;
+            if (!strncmp(buf, views[i].str, val)) {
+                //printf("%ld %ld %ld\n", (long)val, (long)views[i].sz, (long)i);
+                buf += val;
+                break;
+            }
+        }
+
+        if(i >= THREAD_AMOUNT) {
             return -1;
+        }
     }
-
     return 0;
 }
 
 int main() {
+    srand((unsigned int)time(NULL));
     const char *path1 = "/f1";
 
     /* First we test if 2 threads don't conflict with writing simulaneosly. */
@@ -71,17 +76,20 @@ int main() {
     int f1 = tfs_open(path1, TFS_O_CREAT);
     assert(f1 != -1);
 
-    pthread_t t1, t2, t3;
+    pthread_t t[THREAD_AMOUNT];
 
-    assert(pthread_create(&t1, NULL, t1_func, (void *)&f1) == 0);
-    assert(pthread_create(&t2, NULL, t2_func, (void *)&f1) == 0);
-    assert(pthread_create(&t3, NULL, t3_func, (void *)&f1) == 0);
+    handle_index hidx[THREAD_AMOUNT];
 
-    assert(pthread_join(t1, NULL) == 0);
-    assert(pthread_join(t2, NULL) == 0);
-    assert(pthread_join(t3, NULL) == 0);
+    for(int i = 0; i < THREAD_AMOUNT; ++i) {
+        hidx[i].handle = f1;
+        hidx[i].index = i;
+        assert(pthread_create(&t[i], NULL, t_func, (void *)&hidx[i]) == 0);    
+    }
 
-    const int len = 1000;
+    for(int i = 0; i < THREAD_AMOUNT; ++i)
+        assert(pthread_join(t[i], NULL) == 0);
+
+    const int len = 420 * THREAD_AMOUNT * 7;
 
     char buf[len];
 
@@ -90,10 +98,11 @@ int main() {
     /* Abre outra. */
     f1 = tfs_open(path1, 0);
 
-    assert(tfs_read(f1, buf, len - 1) != -1);
+    ssize_t sz = tfs_read(f1, buf, len - 1);
+    assert(sz != -1);
 
     /* Testar se não há interferência de escritas. */
-    assert(test_write_interference(buf, len - 1) != -1);
+    assert(test_write_interference(buf, (size_t)(sz - 1)) != -1);
 
     buf[len - 1] = '\0';
     puts(buf);
